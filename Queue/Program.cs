63f976ef56b989010e02268c3beb094a9629bf86
Rezana <ganierezana8@gmail.com>
2019-08-07 22:25:52 +0200

    feat: enqueue dequeue example

---
+++ Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> numbers = new Queue<string>();
+            numbers.Enqueue("one");
+            numbers.Enqueue("two");
+            numbers.Enqueue("three");
+            numbers.Enqueue("four");
+            numbers.Enqueue("five");
+
+            foreach (string num in numbers)
+            {
+                Console.Write($" {num}, ");
+            }
+
+            Console.WriteLine("\nDequeuing '{0}'", numbers.Dequeue());
+            Console.WriteLine();
+            Console.WriteLine("Peek at next item to dequeue: {0}", 
+                numbers.Peek());
+            Console.WriteLine();
+
+            foreach (string num in numbers)
+            {
+                Console.Write($" {num}, ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
+
+            foreach (string num in numbers)
+            {
+                Console.Write($" {num}, ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
